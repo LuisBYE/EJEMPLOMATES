16c3fdde871686e852d7cacb2d05059f309ca6ef
luis <luis_alexissanchez@ceroca.cat>
2024-09-23 19:55:38 +0200

    MovimientoJugador

---
+++ Assets/Scripts/NuevoJugador.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuevoJugador : MonoBehaviour
+{
+
+    private float _velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _velocidad = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccionIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccionIndicadaY = Input.GetAxisRaw("Vertical");
+        //Debug.Log("X :" + direccionIndicadaX + " - Y: " + direccionIndicadaY);
+        Vector2 direccionIndicada = new Vector2(direccionIndicadaX, direccionIndicadaY);
+        
+        Vector2 navePos = transform.position;// posicion actual de la mavae con transfomr.position
+        navePos = navePos + direccionIndicada * _velocidad * Time.deltaTime;
+        //Debug.Log(Time.deltaTime); //
+
+        transform.position = navePos;
+
+    }
+}
