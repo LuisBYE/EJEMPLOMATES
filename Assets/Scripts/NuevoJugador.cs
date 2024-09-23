3b7d3b0ef6f039de06c6f629c448bfe0cdeb5a42
luis <luis_alexissanchez@ceroca.cat>
2024-09-23 20:26:14 +0200

    LimitePantalla

--- Assets/Scripts/NuevoJugador.cs
+++ Assets/Scripts/NuevoJugador.cs
@@ -6,11 +6,16 @@ public class NuevoJugador : MonoBehaviour
 {
 
     private float _velocidad;
+    private Vector2 minPantalla;
+    private Vector2 maxPantala;
 
     // Start is called before the first frame update
     void Start()
     {
         _velocidad = 8f;
+        minPantalla = Camera.main.ViewportToWorldPoint( new Vector2(0, 0));
+        maxPantala = Camera.main.ViewportToWorldPoint( new Vector2(1, 1));
+
     }
 
     // Update is called once per frame
@@ -18,12 +23,17 @@ public class NuevoJugador : MonoBehaviour
     {
         float direccionIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccionIndicadaY = Input.GetAxisRaw("Vertical");
+
         //Debug.Log("X :" + direccionIndicadaX + " - Y: " + direccionIndicadaY);
+
         Vector2 direccionIndicada = new Vector2(direccionIndicadaX, direccionIndicadaY).normalized;
         
         Vector2 navePos = transform.position;// posicion actual de la mavae con transfomr.position
         navePos = navePos + direccionIndicada * _velocidad * Time.deltaTime;
         //Debug.Log(Time.deltaTime); //
+
+        navePos.X = Mathf.Clamp(navePos.x , minPantalla.x, maxPantala.x);
+        navePos.y = Mathf.Clamp(navePos.y, minPantalla.y, maxPantala.y);
 
         transform.position = navePos;
         
