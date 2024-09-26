77a595de090705927064be131d8452a5a6e1bee8
luis <luis_alexissanchez@ceroca.cat>
2024-09-26 15:49:18 +0200

    Liminit Patalla amb mida Nau

--- Assets/Scripts/NuevoJugador.cs
+++ Assets/Scripts/NuevoJugador.cs
@@ -7,17 +7,30 @@ public class NuevoJugador : MonoBehaviour
 
     private float _velocidad;
     private Vector2 minPantalla;
-    private Vector2 maxPantala;
+    private Vector2 maxPantalla;
 
     // Start is called before the first frame update
     void Start()
     {
+       
         _velocidad = 8f;
-        minPantalla = Camera.main.ViewportToWorldPoint( new Vector2(0, 0));
-        maxPantala = Camera.main.ViewportToWorldPoint( new Vector2(1, 1));
+      
+        maxPantalla = Camera.main.ViewportToWorldPoint( new Vector2(1, 1));// abajo //izquierda
+        minPantalla = Camera.main.ViewportToWorldPoint( new Vector2(0, 0));// arriba //derecha
+
+        float mitatdeimagenX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
+        float mitatdeimagenY = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.y / 2;
+
+        minPantalla.x = minPantalla.x + mitatdeimagenX;
+        maxPantalla.x = maxPantalla.x - mitatdeimagenX;
+        minPantalla.y += mitatdeimagenY;
+        maxPantalla.y -= mitatdeimagenY;
+
+        
+        
 
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -32,11 +45,12 @@ public class NuevoJugador : MonoBehaviour
         navePos = navePos + direccionIndicada * _velocidad * Time.deltaTime;
         //Debug.Log(Time.deltaTime); //
 
-        navePos.X = Mathf.Clamp(navePos.x , minPantalla.x, maxPantala.x);
-        navePos.y = Mathf.Clamp(navePos.y, minPantalla.y, maxPantala.y);
+        //BORDES DE PANTALLA 
+        navePos.x = Mathf.Clamp(navePos.x , minPantalla.x, maxPantalla.x);
+        navePos.y = Mathf.Clamp(navePos.y, minPantalla.y, maxPantalla.y);
 
         transform.position = navePos;
         
-
+        
     }
 }
